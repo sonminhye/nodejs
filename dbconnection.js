a97e95800b23efcd2113539c78242346aaaf7003
minhye <smhgood3@gmail.com>
2017-06-19 11:10:14 +0900

    dbconnection change ip/port

--- dbconnection.js
+++ dbconnection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var mysql_con = mysql.createConnection({
 	host : 'localhost',
 	port : 3306,
-	user : 'root',
+	user : 'project',
 	password : '1234',
 	database : 'travel'
 });
